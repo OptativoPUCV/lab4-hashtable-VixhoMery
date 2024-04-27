ab5dc97f4d540c0d6c5d5b2eecfea94bc7be4ca1
VixhoMery <129796841+VixhoMery@users.noreply.github.com>
2024-04-27 21:59:17 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -136,12 +136,27 @@ Pair * searchMap(HashMap * map,  char * key)
 
 Pair * firstMap(HashMap * map) 
 {
-
+  for(long i = 0; i < map->capacity; i++)
+    {
+      if(map->buckets[i] != NULL && map->buckets[i]->key != NULL)
+      {
+        map->current = i;
+        return map->buckets[i];
+      }    
+    }
     return NULL;
 }
 
 Pair * nextMap(HashMap * map) 
 {
+  for(long i = map->current + 1; i < map->capacity; i++)
+  {
+    if(map->buckets[i] != NULL && map->buckets[i]->key != NULL)
+    {
+      map->current = i;
+      return map->buckets[i];
+    }
+  }
+  return NULL;
 
-    return NULL;
 }
